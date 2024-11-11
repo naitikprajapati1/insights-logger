6019ef73522496679dd3f4b41bf529cba94fa0b3
naitikprajapati1 <naitikprajapti.dev@gmail.com>
2024-11-11 14:51:43 +0530

    feat: add constants for log types and levels

---
+++ src/constant.js
@@ -0,0 +1,16 @@
+export const LOG_TYPES = {
+	API: "api",
+	SYSTEM: "system",
+	DATABASE: "db",
+	CACHE: "cache",
+	SECURITY: "security",
+	PERFORMANCE: "performance",
+	CUSTOM: "custom",
+};
+
+export const LOG_LEVELS = {
+	ERROR: 0,
+	WARN: 1,
+	INFO: 2,
+	DEBUG: 3,
+};
